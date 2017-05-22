e269ff0f9065d2dfa31dab52bbeec3c9d70a157f
kirito96 <eagle1968@live.com>
2017-05-22 16:38:02 -0400

    Remove unnecessary files

--- repackboot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-time=$(date +"%Y%m%d-%H%M");
-cd ~/android/kernel/5010/boot
-cp ../kernel/arch/arm/boot/zImage-dtb boot/split_img/boot.img-zImage
-./repackimg.sh
-rm -r ramdisk-new.cpio.gz
-mv image-new.img boot_$time.img
-echo -e "Your new boot is boot_$time.";
